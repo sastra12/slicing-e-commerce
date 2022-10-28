56c78d0c40538e9f2e17f4fa01c4dfea7ec1b73f
sastra12 <anonymousonly594@gmail.com>
2022-10-28 20:56:42 +0700

    make header, navbar, banner section, trending items

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
     './public/**/*.{html,js}'
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '976px',
+      xl: '1200px',
+    },
+    container: {
+      center: true,
+      padding: '1rem'
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"]
+      },
+      colors: {
+        'primary': '#183B56',
+        'secondary': '#36B37E'
+      },
+    },
   },
   plugins: [],
 }
